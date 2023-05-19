66a65e0e0b03271276fecd91d04fd4533675ea7f
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-19 10:13:06 +0100

    Update header.php

--- header.php
+++ header.php
@@ -11,12 +11,14 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 <!doctype html>
 <html>
 <head>
-    
+    <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=10; IE=9; IE=8; IE=7; IE=EDGE" /> 
+ <meta name="referrer" content="no-referrer" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>FeluxShop</title>
     <link rel="stylesheet" href="files/bootstrap/3/css/bootstrap.css?1" />
-    <link rel="stylesheet" href="layout/css/bootstrap.css" />
-    <link rel="stylesheet" href="layout/css/bootstrap.css" />
-    <link rel="stylesheet" href="layout/css/bootstrap.css" />
-        <link rel="stylesheet" href="layout/css/bootstrap.css" />
+    <link rel="stylesheet" href="layout/css/main.css" />
+    <link rel="stylesheet" href="layout/css/util.css" />
 
     <link rel="stylesheet" href="files/bootstrap/3/css/bootstrap.css?1" />
     <link rel="stylesheet" href="files/bootstrap/3/css/bootstrap.css?1" />
@@ -25,6 +27,11 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 
 <link rel="stylesheet" href="files/css/flags.css" />
 
+<Link tel="stylesheet" 
+    
+                  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+    
+    />
 
 
 <script type="text/javascript" src="files/js/jquery.js?1"></script>
@@ -35,12 +42,8 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 <script type="text/javascript" src="files/js/clipboard.min.js"></script>
 
 <link rel="shortcut icon" href="files/img/favicon.ico" />
-<meta http-equiv="X-UA-Compatible" content="IE=10; IE=9; IE=8; IE=7; IE=EDGE" /> 
- <meta name="referrer" content="no-referrer" />
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta charset="utf-8">
-<title>FeluxShop</title>
+
 </head>
 
 
