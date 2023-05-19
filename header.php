ffc05e7ada545b6c7c9c3ad5267fd149e79fed3d
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-19 10:41:52 +0100

    Update header.php

--- header.php
+++ header.php
@@ -28,8 +28,8 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
       
    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" />
-     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" />
+     <script src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
      <link rel="stylesheet" href="files/css/flags.css" />
       <Link tel="stylesheet" 
                   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
@@ -38,7 +38,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 
 <script src="files/js/jquery.js"></script>
 <script src="layout/js/bootstrap.min.js"></script>
-<script src="js/jquery.datatables.min.js"></script>
+<script src="js/jquery.dataTables.min.js"></script>
 <script src="layout/js/bootbox.min.js"></script>
 <script src="layout/js/clipboard.min.js"></script>
 
