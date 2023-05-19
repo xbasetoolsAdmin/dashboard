9bd9e3ef0875c03807fd4592db3308936eab662d
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-19 15:23:26 +0100

    Update leads.php

--- leads.php
+++ leads.php
@@ -1,147 +1,84 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <div class="d-flex flex-row-reverse mt-0">
-        <div class="p-2">
-            <label id="switch" class="switch">
-                <input type="checkbox" onchange="toggleTheme()" id="slider">
-                <span class="slider round">
-                </span>
-            </label>
-        </div>
-    </div>
-    <div class="alert alert-info text-left" role="alert" style="margin: 15px;">
-        <ul>
-            <li>For Any problem for account after buy just open report and seller will fix it or replace.</li>
-            <li>There is <b> 6 </b> Accounts Available.</li>
-        </ul>
-    </div>
-    <input type=hidden id="cat" name="cat" value="2" />
-    <div class="row m-3 pt-1" style="color: var(--font-color);">
-        <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
-            <label for="infos" style="margin-bottom: 10px; margin-top: 5px">Website Name :</label>
-            <select name="sitename" id="sitename" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
-                <option value="">All</option>
-                <option value="https://my.bluehost.com/web-ho">https://my.bluehost.com/web-ho</option>
-                <option value="https://portal.azure.com">https://portal.azure.com</option>
-                <option value="https://signin.aws.amazon.com/">https://signin.aws.amazon.com/</option>
-                <option value="https://uk.hosting24.com/cpane">https://uk.hosting24.com/cpane</option>
-            </select>
-        </div>
-        <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
-            <label for="infos" style="margin-bottom: 10px; margin-top: 5px">Details:</label>
-            <input type="search" class="form-control" id="infos" style="color: var(--font-color); background-color: var(--color-card);">
-        </div>
-        <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
-            <label for="Country" style="margin-bottom: 10px; margin-top: 5px">Country :</label>
-            <select name="country" id="country" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
-                <option value="">All Countries</option>
-                <option value="United Kingdom">United Kingdom</option>
-                <option value="United States">United States</option>
-            </select>
-        </div>
-        <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
-            <label for="seller" style="margin-bottom: 10px; margin-top: 5px">Seller :</label>
-            <select name="seller" id="seller" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
-                <option value="">All</option>
-                <option value="Seller349">Seller349</option>
-                <option value="Seller327">Seller327</option>
-                <option value="Seller342">Seller342</option>
-            </select>
-        </div>
-    </div>
-    <div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-        <div class="col-sm-12 table-responsive">
-            <table id="account_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
-                <thead>
-                    <tr>
-                        <th data-priority="1"></th>
-                        <th class="all">ID</th>
-                        <th data-priority="3">Website Name</th>
-                        <th data-priority="4">Country</th>
-                        <th data-priority="7">Details</th>
-                        <th data-priority="8">Price</th>
-                        <th data-priority="9">Seller</th>
-                        <th data-priority="10">Source</th>
-                        <th class="all">Proof</th>
-                        <th data-priority="11">Date Created</th>
-                        <th class="all">Buy</th>
-                    </tr>
-                </thead>
-            </table>
-        </div>
-    </div>
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
-        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-notify modal-success" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <p class="heading" id="myModalHeader"></p>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true" class="white-text">×</span>
-                    </button>
-                </div>
-                <div class="modal-body" id="modelbody">
-                </div>
-                <div class="modal-footer justify-content-center">
-                    <a type="button" class="btn btn-outline-success waves-effect" data-dismiss="modal">Close</a>
-                </div>
-            </div>
-        </div>
-    </div>
+<!DOCTYPE html>
+<html>
  
-    <div class="modal fade" id="modalConfirmBuy" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-sm modal-notify modal-info" role="document">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="alfacoins-site-verification" content="5ef8c2279aa605ef8c2279aa965ef8c2279aacb_ALFAcoins">
+    <meta name="revisit-after" content="2 days">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">    <link rel="shortcut icon" href="../../favicon.ico" />
+    <title>Accounts</title>
+    <link rel="stylesheet" href="layout/css/bootstrap.min.css">
+        
+        
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="layout/js/clipboard.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+   
+     <script src="layout/js/bootstrap.min.js"></script>
+    
+    <script src="layout/js/bootbox.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="layout/css/flags.css" />
  
-            <div class="modal-content text-center">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.colVis.min.js"></script>
+    <script src="js/jquery.dataTables.min.js"></script>
+    <link href="//cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
  
-                <div class="modal-header d-flex justify-content-center">
-                    <p class="heading">Are you sure?</p>
-                </div>
+    <script async src="//www.googletagmanager.com/gtag/js?id=UA-177092549-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
  
-                <div class="modal-body">
-                    <i class='fas fa-shopping-cart fa-4x animated rotateIn'></i>
-                </div>
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('set', {
+            '$usrid': 'USER_ID'
+        }); // Set the user ID using signed-in user_id.
+        gtag('config', 'UA-177092549-1');
+    </script>
+    <link rel="stylesheet" href="layout/css/all.min.css" />
+    <link rel="stylesheet" href="layout/css/main.css?v=12.9" />
+    <link rel="stylesheet" href="layout/css/util.css" />
+    <style>
+        body {
+            padding-top: 80px
+        }
+    </style>
+    <link rel="stylesheet" href="layout/fonts/iconic/css/material-design-iconic-font.min.css">
+    <script src="layout/js/main.js"></script>
+    <script type="text/javascript">
+        // Notice how this gets configured before we load Font Awesome
+        window.FontAwesomeConfig = {
+            autoReplaceSvg: false
+        }
+    </script>
+    <style>
+        @import url(//fonts.googleapis.com/css?family=Roboto:400);
  
-                <div class="modal-footer flex-center">
-                    <a onClick='confirmbye()' class="btn btn-outline-info waves-effect" data-dismiss="modal">Yes</a>
-                    <a type="button" class="btn btn-info" data-dismiss="modal">No</a>
-                </div>
-            </div>
- 
-        </div>
-    </div>
- 
- 
-    <div class="modal fade top" id="modalCoupon" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
-        <div class="modal-dialog modal-frame modal-top modal-notify modal-danger" role="document">
- 
-            <div class="modal-content">
- 
-                <div class="modal-body">
-                    <div class="row d-flex justify-content-center align-items-center">
-                        <img src="layout/images/balance.png">
-                        <span class="pt-3 mx-4" style="font-size: 14 px"><b>No enough balance !</b> Please refill your balance</span>
-                        <a type="button" href="addBalance" onclick="window.open(this.href);return false;" class="btn btn-danger">Add Balance
-                            <i class="fas fa-book ml-1 white-text"></i>
-                        </a>
-                        <a type="button" class="btn btn-outline-danger waves-effect" data-dismiss="modal">No, thanks</a>
-                    </div>
-                </div>
-            </div>
- 
-        </div>
-    </div>
- 
+        .navbar-nav .dropdown-menu {
+            margin: 0 !important
+        }
+    </style>
+</head>
+
+
+
+
+
+
+
+
+
+
+
 
 
 
