ba09f74166b40e6b359398e234593f4879ba2892
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-19 11:06:24 +0100

    Update header.php

--- header.php
+++ header.php
@@ -34,15 +34,30 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
       <Link tel="stylesheet" 
                   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
     />
-
-
-<script src="files/js/jquery.js"></script>
 <script src="layout/js/bootstrap.min.js"></script>
-<script src="js/jquery.dataTables.min.js"></script>
+<script src="js/jquery.datatables.min.js"></script>
 <script src="layout/js/bootbox.min.js"></script>
 <script src="layout/js/clipboard.min.js"></script>
-
-
+    <style>
+        body {
+            padding-top: 80px
+        }
+    </style>
+    <link rel="stylesheet" href="layout/fonts/iconic/css/material-design-iconic-font.min.css">
+    <script src="layout/js/main.js"></script>
+    <script type="text/javascript">
+        // Notice how this gets configured before we load Font Awesome
+        window.FontAwesomeConfig = {
+            autoReplaceSvg: false
+        }
+    </script>
+    <style>
+        @import url(//fonts.googleapis.com/css?family=Roboto:400);
+ 
+        .navbar-nav .dropdown-menu {
+            margin: 0 !important
+        }
+    </style>
 
 </head>
 
