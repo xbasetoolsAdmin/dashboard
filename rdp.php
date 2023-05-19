3c9d509c0e3d240b39382a8c4cf95baa439b3885
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-19 11:12:36 +0100

    Update rdp.php

--- rdp.php
+++ rdp.php
@@ -3,43 +3,7 @@
 include "header.php";
 
 ?>
-<style>
-    .display td {
-        background: var(--color-card);
-        color: var(--font-color);
- 
-    }
- 
-    .dataTables_paginate .paginate_button.disabled:active {
- 
-        color: var(--font-color);
- 
-    }
- 
-    .alert-info {
-        color: var(--color-info);
-        background-color: var(--color-backinfo);
-        border-color: var(--color-borderinfo);
- 
-    }
- 
-    #rdp_data_filter {
-        color: var(--font-color);
-    }
- 
-    #rdp_data_length {
-        color: var(--font-color);
-    }
- 
-    #rdp_data_paginate {
-        color: var(--font-color);
-    }
- 
-    #rdp_data_info {
-        color: var(--font-color);
-    }
-</style>
- <?php
+style>.display td{background:var(--color-card);color:var(--font-color)}.dataTables_paginate .paginate_button.disabled:active{color:var(--font-color)}.alert-info{color:var(--color-info);background-color:var(--color-backinfo);border-color:var(--color-borderinfo)}#rdp_data_filter{color:var(--font-color)}#rdp_data_length{color:var(--font-color)}#rdp_data_paginate{color:var(--font-color)}#rdp_data_info{color:var(--font-color)}</style> <?php
 
 include "body/stylesheet_css.php";
 
