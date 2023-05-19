a2b56e4ac84755b5ccbad7387f9abfd2a99a762c
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-19 11:10:39 +0100

    Update rdp.php

--- rdp.php
+++ rdp.php
@@ -1,80 +1,50 @@
 <?php
-ob_start();
-session_start();
-date_default_timezone_set('UTC');
-include "includes/config.php";
 
-if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
-    header("location: ../");
-    exit();
-}
-$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+include "header.php";
+
 ?>
-<!doctype html>
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href="files/bootstrap/3/css/bootstrap.css?1" />
-<link rel="stylesheet" type="text/css" href="files/css/flags.css" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
-<script type="text/javascript" src="files/js/jquery.js?1"></script>
-<script type="text/javascript" src="files/bootstrap/3/js/bootstrap.js?1"></script>
-<script type="text/javascript" src="files/js/sorttable.js"></script>
-<script type="text/javascript" src="files/js/table-head.js?3334"></script>
-<script type="text/javascript" src="files/js/bootbox.min.js"></script>
-<script type="text/javascript" src="files/js/clipboard.min.js"></script>
-
-<link rel="shortcut icon" href="files/img/favicon.ico" />
-<meta http-equiv="X-UA-Compatible" content="IE=10; IE=9; IE=8; IE=7; IE=EDGE" /> 
- <meta name="referrer" content="no-referrer" />
-
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta charset="utf-8">
-<title>JeruxShop</title>
-</head>
 <style>
-#table {
-  .sortable
-}
-table th:not(.sorttable_sorted):not(.sorttable_sorted_reverse):not(.sorttable_nosort):after { 
-    content: " \25BE" 
-}
-
-.label-as-badge {
-    border-radius: 0.5em;
-}
-
-body {
-    padding-top:50px;
-}
-table.floatThead-table {
-    border-top: none;
-    border-bottom: none;
-    background-color: #fff;
-}
-@media (min-width: 768px) {
-  .dropdown:hover .dropdown-menu {
-    display: block;
-  }
-}
-
-#mydiv {
-  height: 400px;
-  position: relative;
-}
-.ajax-loader {
-  position: absolute;
-  left: 0;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  margin: auto; /* presto! */
-
-}
-
-   
-    
-
+    .display td {
+        background: var(--color-card);
+        color: var(--font-color);
+ 
+    }
+ 
+    .dataTables_paginate .paginate_button.disabled:active {
+ 
+        color: var(--font-color);
+ 
+    }
+ 
+    .alert-info {
+        color: var(--color-info);
+        background-color: var(--color-backinfo);
+        border-color: var(--color-borderinfo);
+ 
+    }
+ 
+    #rdp_data_filter {
+        color: var(--font-color);
+    }
+ 
+    #rdp_data_length {
+        color: var(--font-color);
+    }
+ 
+    #rdp_data_paginate {
+        color: var(--font-color);
+    }
+ 
+    #rdp_data_info {
+        color: var(--font-color);
+    }
 </style>
+ <?php
+
+include "body/stylesheet_css.php";
+
+?>
+
 <script type="text/javascript">
              function ajaxinfo() {
                 $.ajax({
